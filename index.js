3ac9f7e8ecc21c9365463488bf2b8b13abf7ccae
lendle <lendle_tseng@seed.net.tw>
2019-06-25 21:12:03 +0800

    step 3

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ var qLocator=webdriver.By.name("q");
 driver.wait(webdriver.until.elementLocated(qLocator), 3000);
 var qElement=driver.findElement(qLocator);
 qElement.sendKeys("Hello!");
+var submitButtonLocator=webdriver.By.name("btnK");
+var submitButton=driver.findElement(submitButtonLocator);
+submitButton.click();
+//會發生錯誤！
+var rcLocator=webdriver.By.className("rc");
+driver.wait(webdriver.until.elementLocated(rcLocator), 10000);
+var rcElements=driver.findElements(rcLocator);
+console.log(rcElements);
