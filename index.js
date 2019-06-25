053f7ac34ad298544f00ec756338b8b91cd3fe12
lendle <lendle_tseng@seed.net.tw>
2019-06-25 21:46:18 +0800

    step 4

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const DRIVER="geckodriver"; //chromedriver
-const BROWSER="firefox"; //chrome
+const DRIVER = "geckodriver"; //chromedriver
+const BROWSER = "firefox"; //chrome
 
 var webdriver = require("selenium-webdriver");
 require(DRIVER);
 
-var builder = new webdriver.Builder();
-builder.forBrowser(BROWSER);
-var driver=builder.build();
-driver.get("http://www.google.com.tw");
-var qLocator=webdriver.By.name("q");
-driver.wait(webdriver.until.elementLocated(qLocator), 3000);
-var qElement=driver.findElement(qLocator);
-qElement.sendKeys("Hello!");
-var submitButtonLocator=webdriver.By.name("btnK");
-var submitButton=driver.findElement(submitButtonLocator);
-submitButton.click();
-//會發生錯誤！
-var rcLocator=webdriver.By.className("rc");
-driver.wait(webdriver.until.elementLocated(rcLocator), 10000);
-var rcElements=driver.findElements(rcLocator);
-console.log(rcElements);
+async function execute() {
+    var builder = new webdriver.Builder();
+    builder.forBrowser(BROWSER);
+    var driver = builder.build();
+    await driver.get("http://www.google.com.tw");
+    var qLocator = webdriver.By.name("q");
+    await driver.wait(webdriver.until.elementLocated(qLocator), 3000);
+    var qElement = await driver.findElement(qLocator);
+    await qElement.sendKeys("Hello!");
+    var submitButtonLocator = webdriver.By.name("btnK");
+    await driver.wait(webdriver.until.elementLocated(submitButtonLocator), 3000);
+    var submitButton = await driver.findElement(submitButtonLocator);
+    await submitButton.click();
+
+    var rcLocator = webdriver.By.className("rc");
+    await driver.wait(webdriver.until.elementLocated(rcLocator), 10000);
+    var rcElements = await driver.findElements(rcLocator);
+    for(let rcElement of rcElements){
+        console.log(await rcElement.getText());
+    }
+    driver.close();
+}
+
+execute();
